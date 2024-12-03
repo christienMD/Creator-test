5b01017a6850618bc0b1756e41cc8f305d5ec454
Md Christien <mdchristien12@gmail.com>
2024-12-03 18:09:28 +0100

    update vercel . js and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
