699603b60298122ab32ee29011807c1235d40f9c
Md Christien <mdchristien12@gmail.com>
2024-12-04 11:11:11 +0100

    clear cart after successfull purchase

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,15 +1,27 @@
-import { useState, useEffect } from 'react';
-import SuccessPage from '@/components/sections/SuccessPage/SuccessPage';
-import { Product } from '@/types/entities';
+import { useState, useEffect } from "react";
+import SuccessPage from "@/components/sections/SuccessPage/SuccessPage";
+import { Product } from "@/types/entities";
+import useCartStore from "@/stores/useCartStore";
 
 function Success() {
   const [cartItems, setCartItems] = useState<Product[]>([]);
+  const clearCart = useCartStore((state) => state.clearCart);
 
   useEffect(() => {
-    // Simulate fetching data (replace this with actual API call or state retrieval)
-    const items = JSON.parse(localStorage.getItem('cartItems') || '[]');
+    // Get cart items from localStorage
+    const items = JSON.parse(localStorage.getItem("cartItems") || "[]");
     setCartItems(items);
-  }, []);
+
+    const timeoutId = setTimeout(() => {
+      clearCart();
+      setCartItems([]);
+      localStorage.removeItem("cartItems");
+    }, 30000); // 30 seconds
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [clearCart]);
 
   return <SuccessPage cartItems={cartItems} />;
 }
